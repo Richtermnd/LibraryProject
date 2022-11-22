fb127fbcc486b7d24b7bf5f761a8e3aab98896f8
”mrust” <m.rustam1305@gmail.com>
2022-11-22 16:58:37 +0500

    add export from ClientInfo.py

--- info_widgets/ClientInfo.py
+++ info_widgets/ClientInfo.py
@@ -1,6 +1,8 @@
+import csv
 from collections import namedtuple
 import sqlite3
-from PyQt5.QtWidgets import QWidget, QTableWidgetItem
+
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QFileDialog
 from PyQt5 import uic, QtCore
 
 
@@ -22,13 +24,14 @@ class ClientInfo(QWidget):
                                    book.id = operation.book AND
                                    operation_type.id = operation.type order by operation.id desc;
                          """
-
+        self.client_name, = self.con.cursor().execute(f'SELECT name FROM client WHERE id = {client_id}').fetchone()
         self._initUI()
         self.update_main_table()
 
     def _initUI(self):
         self.comboBox_mode.currentTextChanged.connect(self.update_main_table)
         self.input_search.textChanged.connect(self.update_main_table)
+        self.export_button.clicked.connect(self.export)
 
     def update_main_table(self) -> None:
         """ Update main table"""
@@ -48,6 +51,23 @@ class ClientInfo(QWidget):
                 self.table_main.setItem(
                     i, j, item)
         self.table_main.resizeColumnsToContents()
+
+    def export(self):
+        filename, _ = QFileDialog.getSaveFileName(self, 'Экспортировать как',
+                                                  rf'C:/Users/{self.comboBox_mode.currentText()}_{self.client_name}',
+                                                  'CSV files (*.csv)')
+        with open(filename, mode='w', newline='') as f:
+            writer = csv.writer(f, delimiter=';',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(
+                [self.table_main.horizontalHeaderItem(i).text()
+                 for i in range(self.table_main.columnCount())])
+
+            for i in range(self.table_main.rowCount()):
+                row = []
+                for j in range(self.table_main.columnCount()):
+                    row.append(self.table_main.item(i, j).text())
+                writer.writerow(row)
 
     def search(self, table: list[list[str]]) -> list[list[str]]:
         text = self.input_search.text().lower()
